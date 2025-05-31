53e1f72afa19efc094fdc70899f27b3ca454c9f2
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-06-01 00:57:42 +0530

    added some code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,31 @@
-
+const user = require('../models/userModel')
+exports.sendToken = async(user,statusCode, res) =>{
+    const token = user.getSignedToken(res)
+    res.status(statusCode).json({
+        success: true,
+        token,
+    })
+}
 
 exports.registerController = async () =>{
+    try{
+        const { username, email, password, subscription, customerId } = req.body;
+        const existingEmail = user.findOne({emai:email})
+        if(existingEmail){
+            return next()
+        }
+        await user.create({
+            username:username,
+            email:email,
+            customerId:customerId,
+            password: password,
+            subscription: subscription
+
+        })
+    }catch(error){
+        console.log(error.messsage)
+        next(error)
+    }
 
 }
 
