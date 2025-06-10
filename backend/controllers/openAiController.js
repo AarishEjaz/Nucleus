a496352ff6717618543b93d8d93b114b94316644
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-06-10 21:58:08 +0530

    added new gen-ai

--- backend/controllers/openAiController.js
+++ backend/controllers/openAiController.js
@@ -1,45 +1,4 @@
 
-
-// const OpenAI = require("openai");
-// require("dotenv").config();
-
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// exports.summaryController = async(req,res) =>{
-//     try{
-//         const {text} = req.body
-//         const { data } = await openai.completions.create({
-//           model: "gpt-3.5-turbo-instruct",
-//           prompt: `Summarise this ${text}`,
-//           max_tokens: 500,
-//           temperature: 0,
-//         });
-
-//         if(data){
-//             if(data.choices[0].text){
-//                 return res.status(200).json(data.choices[0].text)
-//             }
-//         }
-//     }catch(err){
-//         console.log(err)
-//         return res.status(404).json({
-//             message:err.message
-//         })
-//     }
-// }
-
-
-
-// async function main() {
-//   const completion = await openai.completions.create({
-
-//   });
-
-//   console.log(completion);
-// }
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv").config();
 
