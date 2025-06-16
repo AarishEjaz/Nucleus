d535c35aa05cbcd82d46dbacd6de552a302508c1
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-06-16 17:13:09 +0530

    changed cors

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    credentials: true
+    origin: "https://nucleus-8n31.onrender.com/",
+    credentials: true,
   })
 );
 app.use(bodyParser.urlencoded({extended:false}))
